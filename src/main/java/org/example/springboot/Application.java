e5314f5c19a93dbc67504af88ae405541f947cdf
somehow55 <somehow55@gmail.com>
2022-07-20 00:48:31 +0900

    220719 build.gradle의 deprecated 부분 수정, Application.java 작성 시작

---
+++ src/main/java/org/example/springboot/Application.java
@@ -0,0 +1,13 @@
+package org.example.springboot;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//오류 뜨는 이유 찾아 수정할 것 (Cannot resolve symbol 'SpringAplication')
+
+//@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        //SpringApplication.run(Applicataion.class, args);
+    }
+}
